294bda01b5dde4cd4b3c199d4354de71ff55bc85
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2024-05-19 01:14:54 +0900

    First Commit

---
+++ src/main/java/org/kangnam/containlaw/ContainLawApplication.java
@@ -0,0 +1,13 @@
+package org.kangnam.containlaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainLawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainLawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kangnam/containlaw/ContainLawApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kangnam.containlaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainLawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
