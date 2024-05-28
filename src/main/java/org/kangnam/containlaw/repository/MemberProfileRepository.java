5a41cfb7260f3e58e54b485fe85e89ef879012b3
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2024-05-28 11:05:15 +0900

    Add: Profile 조회 기능

---
+++ src/main/java/org/kangnam/containlaw/repository/MemberProfileRepository.java
@@ -0,0 +1,12 @@
+package org.kangnam.containlaw.repository;
+
+import org.kangnam.containlaw.entity.MemberProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemberProfileRepository extends JpaRepository<MemberProfile, Long> {
+    List<MemberProfile> findByNameContaining(String name);
+}
