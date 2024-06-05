86622ae39d856f9cf1990e37ddc6b084ea0ee5d4
seowooda <sara0409@naver.com>
2024-06-05 19:08:21 +0900

    Feat: 페이지&컨트롤러 연결

--- src/main/java/org/kangnam/containlaw/entity/MemberProfile.java
+++ src/main/java/org/kangnam/containlaw/entity/MemberProfile.java
@@ -16,25 +16,43 @@ public class MemberProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String name;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String hanjaName;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String englishName;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String calendarType;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String birthDate;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String partyName;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String district;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String mainCommittee;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String reelection;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String elected;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String gender;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String phoneNumber;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String officeNumber;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String email;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String website;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String twitter;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String facebook;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String youtube;
+    @Column(nullable = false, columnDefinition = "DEFAULT ''")
     private String blog;
 
 
