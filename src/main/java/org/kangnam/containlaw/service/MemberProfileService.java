be26c65bac0975d1d5922e03b41751ae2dd38904
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2024-06-07 15:46:42 +0900

    image db 저장 기능

--- src/main/java/org/kangnam/containlaw/service/MemberProfileService.java
+++ src/main/java/org/kangnam/containlaw/service/MemberProfileService.java
@@ -1,13 +1,14 @@
 package org.kangnam.containlaw.service;
 
+import org.kangnam.containlaw.api.Profile.ProfileService;
 import org.kangnam.containlaw.entity.MemberProfile;
 import org.kangnam.containlaw.repository.MemberProfileRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class MemberProfileService implements MemberProfileServiceImpl {
@@ -15,6 +16,10 @@ public class MemberProfileService implements MemberProfileServiceImpl {
 
     @Autowired
     private MemberProfileRepositoryImpl memberProfileRepository;
+
+    @Autowired
+    private ProfileService profileService;
+
 
     public MemberProfileService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
@@ -54,8 +59,25 @@ public class MemberProfileService implements MemberProfileServiceImpl {
         return memberProfileRepository.save(existingProfile);
     }
 
+    @Override
     public MemberProfile getProfileById(Long id) {
-        return memberProfileRepository.findById(id)
+        MemberProfile memberProfile = memberProfileRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Profile not found"));
+        return memberProfile;
     }
+
+    public void updateProfileImageUrl(MemberProfile memberProfile) {
+        String imgPath = profileService.getProfileImgPath(memberProfile.getName());
+        memberProfile.setImageUrl(imgPath);
+        memberProfileRepository.save(memberProfile);
+    }
+
+    @Scheduled(fixedRate = 100000)
+    public void updateAllProfileImages() {
+        List<MemberProfile> profiles = memberProfileRepository.findAll();
+        for (MemberProfile profile : profiles) {
+            updateProfileImageUrl(profile);
+        }
+    }
+
 }
