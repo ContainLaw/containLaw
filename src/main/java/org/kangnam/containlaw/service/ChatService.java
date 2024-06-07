e6f6412e0645972e7f45065fa4806c169e658cd0
daeun <leedaeun973@gmail.com>
2024-06-07 14:54:33 +0900

    Gpt4o Test, Debug, prompt 수정2

--- src/main/java/org/kangnam/containlaw/service/ChatService.java
+++ src/main/java/org/kangnam/containlaw/service/ChatService.java
@@ -27,11 +27,11 @@ public class ChatService {
     public ResponseData getAllInformation(String text) throws IOException, InterruptedException {
         String truncatedText = truncateText(text, 4000);
         String prompt = String.join("\n",
-                "1- 다음 입법안 내용을 이해하기 쉽게 요약해줘(1000자 미만): " + truncatedText,
-                "2- 다음 입법안 내용이 괄호 안 16개 카테고리(경제, 보건, 교육, 환경, 노동, 사회 복지, 법 집행 및 사법, 인권 및 평등, 국제 관계, 안보 및 국방, 부동산 및 토지 관리, 가족 및 개인, 정보 및 통신, 교통 및 물류, 문화 및 예술, 과학 및 기술.) 중 어떤 카테고리에 속하는지 알려줘: " + truncatedText,
-                "3- 입법안이 집행되었을 때의 장점(255자 미만): " + truncatedText,
-                "4- 입법안이 집행되었을 때의 단점(255자 미만): " + truncatedText,
-                "출력방식은 요약내용:, 카테고리:, 장점:, 단점:은 출력되지 않고, 이와 같이 내용만 출력되도록 해줘. 요약내용 \n\n 카테고리 \n\n 장점 \n\n 단점\n\n"
+                "1- 다음 입법안 내용을 이해하기 쉽게 요약해줘(varchar 1000 미만): " + truncatedText,
+                "2- 다음 입법안 내용이 괄호 안 16개 카테고리(경제, 보건, 교육, 환경, 노동, 사회 복지, 법 집행 및 사법, 인권 및 평등, 국제 관계, 안보 및 국방, 부동산 및 토지 관리, 가족 및 개인, 정보 및 통신, 교통 및 물류, 문화 및 예술, 과학 및 기술.) 중 해당하는 카테고리 하나 알려줘. 주의: 오직 이 16개 카테고리 중 하나여야 함.: " + truncatedText,
+                "3- 입법안이 집행되었을 때의 장점(varchar 255 미만): " + truncatedText,
+                "4- 입법안이 집행되었을 때의 단점(varchar 255 미만): " + truncatedText,
+                "출력 결과를 'FLAG'라는 구분자를 사용해서 구분하고 싶어." + "'요약내용 FLAG 카테고리 FLAG 장점 FLAG 단점' 처럼 출력해줘"
         );
 
         String body = objectMapper.writeValueAsString(new OpenAiRequest(prompt));
