c2d67d02dcd0528b1f1fba61d85de89531a5f3bc
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-06-11 00:33:14 +0900

    Update ScheduledService.java

--- src/main/java/org/kangnam/containlaw/service/ScheduledService.java
+++ src/main/java/org/kangnam/containlaw/service/ScheduledService.java
@@ -23,7 +23,7 @@ public class ScheduledService {
     private BillService billService;
     private static final Logger logger = LoggerFactory.getLogger(LsmLegService.class);
 
-    @Scheduled(cron = "0 0 18 * * *")
+    @Scheduled(cron = "0 0 18 * * MON-FRI")
     public void autoUpdateNewLsmLeg() throws IOException, InterruptedException {
         List<LsmLegRes.LsmLeg> lsmLegList = getLsmLegState("3");
         if (lsmLegList != null) {
